37eced44ef5ca86b4c9eae7165f8f1d30677224a
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-25 15:05:53 +0545

    Greeting Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'root#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      # resources :greetings, only: [:index]
+      get '/random_greeting', to: 'greetings#random_greeting'
+    end
+  end
 end
